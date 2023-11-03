36354d407c84f4639b278f39e0d022b7dbfbeebf
IJ <IJ@183.101.187.251>
2023-11-03 22:16:06 +0900

    스크린 활용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,8 +1,32 @@
-
+ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String[] args) {
 		
-		int result = 4;
+		/*int result = 4;
 		System.out.println(result * 2);
+		int resulT = result * 2;
+		System.out.println(resulT * 2);*/
+		
+		System.out.println("계산할 값은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number + "에 곱하고 싶은 숫자는? ");
+        Scanner asdf = new Scanner(System.in);
+        int qwer = asdf.nextInt();
+        
+        System.out.println(number * qwer);
+
+        /*int result = number * 1;                
+        System.out.println("* 1 :" + number * 1 );
+        System.out.println("* 2 :" + number * 2 );
+        System.out.println("* 3 :" + number * 3 );
+        System.out.println("* 4 :" + number * 4 );
+        System.out.println("* 5 :" + number * 5 );
+        System.out.println("* 6 :" + number * 6 );
+        System.out.println("* 7 :" + number * 7 );
+        System.out.println("* 8 :" + number * 8 );
+        System.out.println("* 9 :" + number * 9 );*/
+        // 구구단 출력하는 예제 구현        
 	}
 }
+
