97f240ba13539b8ec9fc1fc1a7d09e23aa5731e5
IJ <IJ@183.101.187.251>
2023-11-06 20:47:23 +0900

    조건문 연습

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -7,10 +7,23 @@ public class Gugudan {
 		int resulT = result * 2;
 		System.out.println(resulT * 2);*/
 		
-		System.out.println("계산할 값은? : ");
+		System.out.println("구구단 중 출력할 값은? : ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println(number + "에 곱하고 싶은 숫자는? ");
+        if(number < 10 && number > 1) {
+        	for(int back = 1; back < 10; back++) {
+        		System.out.println(number * back);
+        	}	
+        }
+        else{
+        	
+        		System.out.println("구구단을 출력할 수 없습니다.");
+        	}
+	
+        
+        
+        
+        /*System.out.println(number + "에 곱하고 싶은 숫자는? ");
         Scanner asdf = new Scanner(System.in);
         int qwer = asdf.nextInt();
         
@@ -25,17 +38,19 @@ public class Gugudan {
         System.out.println("* 6 :" + number * 6 );
         System.out.println("* 7 :" + number * 7 );
         System.out.println("* 8 :" + number * 8 );
-        System.out.println("* 9 :" + number * 9 );
+        System.out.println("* 9 :" + number * 9 );*/
         // 구구단 출력하는 예제 구현      
         
-        int value = 1;
+        /*int value = 1;
         while (value < 10) {
         	System.out.println(6 * value);
         	value = value + 1;
         }
         for(int Value = 1; Value < 10; Value++) {
         System.out.println(7 * Value);
-        }
+        }*/
+        
+        
 	}
 }
 
