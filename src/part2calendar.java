f238dfed6792d2be60fc247dd33236468ccf3805
IJ <IJ@183.101.187.251>
2023-11-22 00:13:27 +0900

    캘린더 파트. 다시 기본부터 하는듯?

---
+++ src/part2calendar.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class part2calendar {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a,b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		System.out.println("a + b의 값은?");
+		System.out.println(a+b +"입니다.");
+		
+	}
+}
