cebeb3090b48724d70a2ba485af318ef44635e6e
IJ <IJ@183.101.187.251>
2023-11-22 21:04:06 +0900

    제대로 된 숫자를 입력해도, 3번에 한해 찾아볼 수 있게끔 하는 법.

--- src/calendar.java
+++ src/calendar.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 public class calendar {
 	public static void main(String[] args) {
 		int chance = 3;
-		System.out.println("최대 날짜를 알고 싶은 달을 지정해주세요." + chance + "번의 기회가 있습니다.");
+		System.out.println("최대 날짜를 알고 싶은 달을 지정해주세요." + chance + "번의 기회가 남았습니다.");
 		int[] maxDays= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		boolean sim = true;
 		while(sim) {
-			
 			Scanner scanner = new Scanner(System.in);
 			int month = scanner.nextInt();
 			if (month < 13) {
 				System.out.println( month + "월은 " + maxDays[month - 1] + "일 까지 있습니다.");
 				//배열의 처음 값은 1번째가 아니라, 0번째 값이므로 입력받은 month 값에서 1을 빼줘야 함.
-				
-				break;
+				chance --;
+				if (chance > 0) {
+					System.out.println("최대 날짜를 알고 싶은 달을 지정해주세요." + chance + "번의 기회가 남았습니다.");
+				continue;
+				}
+				else {
+					System.out.println("모든 기회를 사용하셨습니다. 감사합니다.");
+					break;
+				}
 			}
 			
 			else {
@@ -22,13 +28,10 @@ public class calendar {
 				if (chance == 0) {
 					System.out.println("잘못된 숫자를 3회 지정했습니다. 10분 뒤에 이용해주세요.");
 					break;
-				}
 			}
-			
-			}
-			
-			
-			
 		}
+			scanner.close();
+		}
+	}
 		
 }
