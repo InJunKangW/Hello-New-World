f3fe52d781d49dd696c8df91409456b028195969
IJ <IJ@183.101.187.251>
2023-12-06 23:32:50 +0900

    배열 형태의 result를 갖는 매소드를 만들고, 호출하기.

--- src/PracticeMain.java
+++ src/PracticeMain.java
@@ -3,6 +3,17 @@ public class PracticeMain {
 	void error(){
 		System.out.println("오류가 발생했습니다. 페이지를 새로고침하세요.");
 	}
+	int[] number(){
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		int[] result = new int[number];
+		for(int i=0; i<number; i++) {
+			result[i] = i * i;
+			System.out.println(result[i]);
+		}
+		scanner.close();
+		return result;
+	}
 	
 }
 		
